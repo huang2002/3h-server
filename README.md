e552f391792a771441f970d02cb27d45ee0c8ad3
hhh <1340545944@qq.com>
2018-07-07 22:17:00 +0800

    update

---
+++ README.md
@@ -0,0 +1,27 @@
+# 3h-server
+
+A cli server lib.
+
+# Usage
+
+```
+3h-server  [options]
+
+    -h, --help                  Show help info.
+
+    -p, --port      <port>      The port to listen on.
+                                Default: 80
+
+    -l, --log                   Log request urls.
+
+    -r, --result                Log request results.
+
+    -e, --err                   Log errors.
+
+    -t, --time      <format>    Time format.
+
+    --default-pages <files...>  Additional default pages.
+
+    --sub-routers   <files...>  Additional sub-routers.
+
+```
---
+++ dist/index.js
@@ -0,0 +1,51 @@
+#!node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Logger = require("3h-log");
+const Router = require("3h-router");
+const cli = require("./cli");
+function pick(arr, defaultVal) {
+    return arr && arr.length > 0 ? arr[0] : defaultVal;
+}
+cli.on('exec', args => {
+    if (args.has('h')) {
+        return cli.help();
+    }
+    const router = new Router(), logger = new Logger();
+    if (args.has('t')) {
+        logger.timeFormat = args.get('t').join(' ');
+    }
+    if (args.has('l')) {
+        router.on('before', url => {
+            logger.print('request', url);
+            router.route(url);
+        });
+    }
+    if (args.has('r')) {
+        router.on('result', result => {
+            logger.print('result', result.code.toString());
+        });
+    }
+    if (args.has('e')) {
+        router.on('error', err => {
+            logger.error(err.message);
+        });
+    }
+    const defaultPagesFlag = '-default-pages';
+    if (args.has(defaultPagesFlag)) {
+        const { defaultPages } = router;
+        args.get(defaultPagesFlag).forEach(defaultPage => {
+            defaultPages.unshift(defaultPage);
+        });
+    }
+    const subRoutersFlag = '-sub-routers';
+    if (args.has(subRoutersFlag)) {
+        const { subRouters } = router;
+        args.get(subRoutersFlag).forEach(subRouter => {
+            subRouters.unshift(subRouter);
+        });
+    }
+    const port = +pick(args.get('p'), '80');
+    router.start(port);
+    logger.info(`Server started on port ${port}.`);
+}).exec(process.argv);
